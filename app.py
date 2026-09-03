07933e7e062a537099ba00861d548bdf2e3b5cf3
Mariela <nunezmariela497@gmail.com>
2026-09-03 14:57:50 -0500

    Actualizar pagina AR GLOBAL

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import webbrowser
+from threading import Timer
 
 from flask import Flask, render_template
 
@@ -10,7 +11,13 @@ def inicio():
     return render_template("index.html")
 
 
+def abrir_navegador():
+    webbrowser.open("http://127.0.0.1:5000/")
+
+
 if __name__ == "__main__":
+    Timer(1, abrir_navegador).start()
+
     app.run(
         debug=False,
         host="0.0.0.0",
